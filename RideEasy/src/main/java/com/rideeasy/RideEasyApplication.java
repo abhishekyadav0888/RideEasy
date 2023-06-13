1df443c3f2fb370f33a43714d5185d8e05149799
abhishekyadav0888 <shraddhayadav0888.com>
2023-06-13 16:05:14 +0530

    Project

---
+++ RideEasy/src/main/java/com/rideeasy/RideEasyApplication.java
@@ -0,0 +1,13 @@
+package com.rideeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideEasyApplication.class, args);
+	}
+
+}
