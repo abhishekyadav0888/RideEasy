886a419fbc8e0e00ad93cc92c428d5c504612041
abhishekyadav0888 <shraddhayadav0888.com>
2023-06-19 10:16:33 +0530

    update

--- RideEasy/src/main/java/com/rideeasy/controller/AdminController.java
+++ RideEasy/src/main/java/com/rideeasy/controller/AdminController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,6 @@ import com.rideeasy.model.Admin;
 import com.rideeasy.model.TripBooking;
 import com.rideeasy.service.AdminService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
